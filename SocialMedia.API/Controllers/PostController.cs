8880b314c175c1cf9e67312a134e02255beff669
isfenixrojo <isfenixrojo@mail.com>
2021-03-22 01:38:37 -0600

    Agregar archivos de proyecto.

---
+++ SocialMedia.API/Controllers/PostController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+
+namespace SocialMedia.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _repository;
+
+        public PostController(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPost()
+        {
+            var post = await _repository.GetPost();
+            return Ok(post);
+        }
+    }
+}
---
+++ SocialMedia.Core/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SocialMedia.Core.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Date { get; set; }
+        public string Image { get; set; }
+        public int UserId { get; set; }
+    }
+}
